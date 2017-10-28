f0a0a9ba3271374ce13a590e8cc4050f2d177af5
volcano12 <volcano12@163.com>
2017-10-28 22:32:36 +0800

    New beginning! Learning Again!

---
+++ app/src/main/java/com/example/gcy/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.example.gcy.photogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
