ad804e6d361da5d1b4a38a0d02163cc24b279855
volcano12 <volcano12@163.com>
2017-10-29 21:02:26 +0800

    增加网络调用功能，从url中获取数据

---
+++ app/src/main/java/com/example/gcy/photogallery/FlickrFetchr.java
@@ -0,0 +1,8 @@
+package com.example.gcy.photogallery;
+
+/**
+ * Created by gcy on 2017/10/29.
+ */
+
+public class FlickrFetchr {
+}
---
+++ app/src/main/java/com/example/gcy/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,10 @@
+package com.example.gcy.photogallery;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by gcy on 2017/10/29.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+}
